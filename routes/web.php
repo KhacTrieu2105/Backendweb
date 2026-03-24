5c727250881166a77beb912895d39e25134bd224
DELL <tn8159744@gmail.com>
2026-03-24 12:57:45 +0700

    Upgrade Nexx

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Artisan;
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/init-db', function () {
+    try {
+        Artisan::call('migrate:fresh --force');
+        return "Database đã được khởi tạo và tạo bảng thành công!";
+    } catch (\Exception $e) {
+        return "Lỗi: " . $e->getMessage();
+    }
 });
